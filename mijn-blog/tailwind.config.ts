1550836b2cc5f8e397a3312bf39da51802ee73c6
Ali Soujaa <ali.soujaa@ap.be>
2025-02-18 16:10:08 +0000

    poppins-font-created

--- mijn-blog/tailwind.config.ts
+++ mijn-blog/tailwind.config.ts
@@ -11,6 +11,10 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        
+      },
+      fontFamily: {
+        poppins: ["var(--font-poppins)", "sans-serif"]
       },
     },
   },
