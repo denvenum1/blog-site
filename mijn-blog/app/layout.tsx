2e793ef6930dda3b4dee60ff4d148df4f5aba265
Ali Soujaa <ali.soujaa@ap.be>
2025-02-19 11:59:18 +0000

    updated global.css and doc components

--- mijn-blog/app/layout.tsx
+++ mijn-blog/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,7 +25,9 @@ export default function RootLayout({
       <body
         className={poppins.variable}
       >
+        <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
