61002800023dcd9b13304f051b76586842b7509e
Ali Soujaa <ali_soujaa_2660@hotmail.com>
2025-03-07 09:38:45 +0000

    fixed canary

--- mijn-blog/next.config.ts
+++ mijn-blog/next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: 'incremental'
-  },
-  devIndicators: {
-    appIsrStatus: true,
-    buildActivity: true,
-    buildActivityPosition: 'bottom-right'
-  }
 };
 
 export default nextConfig;
